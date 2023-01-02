916e1fd110a504afa864c375f9db322530e3f2d8
deniz.goezuetok <deniz.goezuetok@gmail.com>
2023-01-02 16:56:47 +0100

    Add: SearchResult directory and cs

---
+++ source/Preisvergleich.FetchData/PageObjectModels/Digitec/SearchResult/DigitecSearchResultPageMap.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using Preisvergleich.FetchData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preisvergleich.FetchData.PageObjectModels.Digitec.SearchResult
+{
+    internal static class DigitecSearchResultPageMap
+    {
+        public static IWebElement GetPrice =>
+            WebUiDriver.wait.Until(ExpectedConditions.ElementExists(By.XPath("//div/article[1]/div[4]/span")));
+    }
+}
