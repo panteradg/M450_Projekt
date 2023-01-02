a0436f2126fa580d8367c323a26113e2bd60b2e4
schned27 <david.schneider.ds4@roche.com>
2023-01-02 18:26:21 +0100

    Created Models for Produkt and Webseite and added the DataLoader

--- source/Preisvergleich.DataAccess/DataLoader.cs
+++ source/Preisvergleich.DataAccess/DataLoader.cs
@@ -1,7 +1,22 @@
-﻿namespace Preisvergleich.DataAccess
+﻿using Newtonsoft.Json;
+using Preisvergleich.DataAccess.Models;
+
+namespace Preisvergleich.DataAccess
 {
     public class DataLoader
     {
+        public DataLoader()
+        {
+            using (StreamReader reader = new StreamReader("../../../../Preisvergleich.DataAccess/inputData.json"))
+            {
+                string json = reader.ReadToEnd();
+                var jsonResult = JsonConvert.DeserializeAnonymousType(json, new { Webseiten = new List<Webseite>(), Produkte = new List<Produkt>() });
+                this.Webseiten = jsonResult.Webseiten;
+                this.Produkte = jsonResult.Produkte;
+            }
+        }
 
+        public List<Webseite> Webseiten { get; }
+        public List<Produkt> Produkte { get; }
     }
 }
---
+++ source/Preisvergleich.DataAccess/Models/ProduktVonWebseite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preisvergleich.DataAccess.Models
+{
+    internal class ProduktVonWebseite
+    {
+        public decimal Preis { get; set; }
+        public Webseite Webseite { get; set; }
+        public Produkt Produkt { get; set; }
+    }
+}
